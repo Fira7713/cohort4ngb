a275a4640ca1786c2762f1831c3eddbf73d11909
Fira7713 <shafira7713@gmail.com>
2019-04-17 11:25:52 +0800

    Add in module

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,8 +1,9 @@
 import {NgModule} from '@angular/core';
 import {ExamplePage} from './example/example.page';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
-  imports:[],
+  imports:[SharedModule],
   exports:[],
   declarations:[ExamplePage],
   providers:[],
