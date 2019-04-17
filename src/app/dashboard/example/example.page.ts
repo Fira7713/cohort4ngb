334b60337d0f2a83393fcc034221f915eb77eea1
Nate C <N_t_N@live.com>
2019-04-17 17:24:33 +0800

    Nate page

--- src/app/dashboard/example/example.page.ts
+++ src/app/dashboard/example/example.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {NgbCalendar, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+import {Country} from './country.model';
 
 @Component({
   selector: 'trg-example-page',
@@ -8,6 +9,33 @@ import {NgbCalendar, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 })
 
 export class ExamplePage {
+
+    countries: Country[] = [
+      {
+        name: 'Russia',
+        flag: 'f/f3/Flag_of_Russia.svg',
+        area: 17075200,
+        population: 146989754
+      },
+      {
+        name: 'Canada',
+        flag: 'c/cf/Flag_of_Canada.svg',
+        area: 9976140,
+        population: 36624199
+      },
+      {
+        name: 'United States',
+        flag: 'a/a4/Flag_of_the_United_States.svg',
+        area: 9629091,
+        population: 324459463
+      },
+      {
+        name: 'China',
+        flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
+        area: 9596960,
+        population: 1409517397
+      }
+    ];
 
   model: NgbDateStruct;
   date: {year: number, month: number};
